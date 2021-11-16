df26abac62e15c96460bbb6fc075421106b0e3d9
Elmir Web <elmir.web@gmail.com>
2021-11-16 23:14:00 +0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+
+import { useState } from "react";
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+const MOVE_VARIANT_NONE = 0; // 0 - не куда не может двигаться
+const MOVE_VARIANT_UP = 1; // 1 - может двигаться на верх
+const MOVE_VARIANT_RIGHT = 2; // 2 - может двигаться вправо
+const MOVE_VARIANT_DOWN = 3; // 3 - может двигаться вниз
+const MOVE_VARIANT_LEFT = 4; // 4 - может двигаться влево
+
+class Field {
+  field = 55;
+  freeMove = MOVE_VARIANT_NONE;
+
+  constructor(element) {
+    this.field = element;
+  }
+}
+
+class Game {
+  one = 0;
+  two = 0;
+  three = 0;
+  four = 0;
+  five = 0;
+  six = 0;
+  seven = 0;
+  eight = 0;
+
+  constructor() {
+    let arrayGame = [1, 2, 3, 4, 5, 6, 7, 8];
+    shuffle(arrayGame);
+
+    console.log(arrayGame);
+    console.log("_________________________");
+
+    let index = 0;
+
+    for (let key in this) {
+      this[key] = new Field(arrayGame[index]);
+      console.log(this[key]);
+      index++;
+    }
+
+    this.six.freeMove = MOVE_VARIANT_DOWN;
+    this.eight.freeMove = MOVE_VARIANT_RIGHT;
+
+    console.log("---------------------");
+    console.log("---------------------");
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  let game = new Game();
+  console.log(game);
+
+  let divArray = [];
+
+  for (let key in game) {
+    divArray.push(
+      <div
+        className="field"
+        onClick={() => {
+          switch (game[key].freeMove) {
+            case MOVE_VARIANT_NONE: {
+              window.alert("Нельзя переместить этот блок");
+              break;
+            }
+
+            case MOVE_VARIANT_UP: {
+              window.alert("вверх");
+              break;
+            }
+            case MOVE_VARIANT_RIGHT: {
+              // продолжить здесь
+              // window.alert("вправо");
+              window.alert(JSON.stringify(game[key], null, 2));
+              break;
+            }
+            case MOVE_VARIANT_DOWN: {
+              window.alert("вниз");
+              break;
+            }
+            case MOVE_VARIANT_LEFT: {
+              window.alert("влево");
+              break;
+            }
+          }
+        }}
+        key={(key + 1).toString()}
+      >
+        <span className="fieldValue">{game[key].field}</span>
+      </div>
+    );
+  }
+
+  divArray.push(
+    <div
+      className="fieldEmpty"
+      onClick={() => {
+        window.alert("empty");
+      }}
+    >
+      <span className="fieldValue">Пусто</span>
     </div>
   );
+
+  console.log(divArray);
+
+  const [stateDivs, setStateDivs] = useState(divArray);
+
+  return <div className="App">{stateDivs}</div>;
 }
 
 export default App;
